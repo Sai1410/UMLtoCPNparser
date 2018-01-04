e0a71dc5c23dd9e7bfb116643022d5240ae8c92b
Pawel Srokowski <pawelsrokowski@Pawels-MacBook-Pro.local>
2018-01-04 15:50:51 +0100

    Started XML

--- src/parser/CPN/XMLCreator.java
+++ src/parser/CPN/XMLCreator.java
@@ -1,11 +1,36 @@
 package parser.CPN;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class XMLCreator {
 
-    public XMLCreator() {
+    public XMLCreator(String filePath) {
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            loadCPNTemplateXML(inputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    private void loadCPNTemplateXML(){
-        
+    private void loadCPNTemplateXML(InputStream inputStream){
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document document = docBuilder.parse(inputStream);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
